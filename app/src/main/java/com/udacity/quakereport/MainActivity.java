4ee3bd927cc3389084fc13ef804106967febe50c
Christos Christidis <christos.christidis86@gmail.com>
2019-08-23 01:11:29 +0300

    Move networking to AsyncTaskLoader + handle bad cases

--- app/src/main/java/com/udacity/quakereport/MainActivity.java
+++ app/src/main/java/com/udacity/quakereport/MainActivity.java
@@ -1,35 +1,127 @@
 package com.udacity.quakereport;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Earthquake>> {
+
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
+    private static final int EARTHQUAKE_LOADER_ID = 1;
+
+    private EarthquakeAdapter mAdapter;
+
+    private TextView mEmptyView;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a fake list of earthquake locations.
-        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
         ListView listView = findViewById(R.id.list);
-        EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
-        listView.setAdapter(adapter);
+        mAdapter = new EarthquakeAdapter(MainActivity.this, new ArrayList<Earthquake>());
+        listView.setAdapter(mAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Earthquake earthquake = earthquakes.get(position);
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(earthquake.getUrl()));
-                startActivity(intent);
+                Earthquake earthquake = mAdapter.getItem(position);
+                if (earthquake != null) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(earthquake.getUrl()));
+                    startActivity(intent);
+                }
             }
         });
+
+        mEmptyView = findViewById(R.id.empty_view);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        if (isConnected) {
+            // SOS: Theoretically, after rotation, the same data is displayed (no downloading), whereas
+            // if I leave the app and return, new data is loaded so we can have fresh data. In practice,
+            // read comments in WhoWroteIt app...
+            getSupportLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this);
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mEmptyView.setText(R.string.no_connection);
+            mEmptyView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @NonNull
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int i, @Nullable Bundle bundle) {
+        // SOS: see WhoWroteIt app for a case where we pass args to this constructor dynamically
+        return new EarthQuakeLoader(this, USGS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<Earthquake>> loader, List<Earthquake> data) {
+        mProgressBar.setVisibility(View.GONE);
+
+        mAdapter.clear();
+
+        if (data != null && !data.isEmpty()) {
+            mAdapter.addAll(data);
+            mEmptyView.setVisibility(View.GONE);
+        } else {
+            mEmptyView.setText(R.string.no_earthquakes_found);
+            mEmptyView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<Earthquake>> loader) {
+        mAdapter.clear();
+    }
+
+    private static class EarthQuakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+        private final String mUrl;
+
+        EarthQuakeLoader(@NonNull Context context, String url) {
+            super(context);
+            mUrl = url;
+        }
+
+        @Override
+        protected void onStartLoading() {
+            forceLoad();
+        }
+
+        @Nullable
+        @Override
+        public List<Earthquake> loadInBackground() {
+            if (TextUtils.isEmpty(mUrl)) {
+                return null;
+            }
+
+            return QueryUtils.fetchEarthquakeData(mUrl);
+        }
     }
 }
